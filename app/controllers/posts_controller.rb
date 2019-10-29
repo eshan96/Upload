6dd474f7c39d8daa208c3ed7f92e799a5293ed6f
Your Name <eshanjindal9@gmail.com>
2019-10-29 13:16:20 +0530

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+    def new
+       @post = Post.new
+    end
+
+    def index
+        @posts = Post.all
+    end
+    
+    def create 
+      @post = Post.create(post_params)
+      redirect_to posts_path
+
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:image)
+    end
+end
